c474150eb98b252485554a6d27447ba847d2aa37
yejin9819 <76990017+yejin9819@users.noreply.github.com>
2021-10-27 20:29:41 +0900

    BOJ 14719 빗물

--- 15주차/211027/BOJ_14719_kyj.java
+++ 15주차/211027/BOJ_14719_kyj.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class baekjoon_14719 {
+public class BOJ_14719_kyj {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());

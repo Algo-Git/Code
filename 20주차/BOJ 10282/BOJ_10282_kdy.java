7ca1c9c5a44774a170ccc99b7a07453100c3392a
k03147 <31230483+k03147@users.noreply.github.com>
2021-12-06 19:57:52 +0900

    BOJ 10282 해킹

--- 20주차/BOJ 10282/BOJ_10282_kdy.java
+++ 20주차/BOJ 10282/BOJ_10282_kdy.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class N10282 {// BOJ 10282. 해킹
+public class BOJ_10282_kdy {// BOJ 10282. 해킹
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

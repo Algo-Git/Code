9dd522022779e190825f233c89a79083658ea8de
DESKTOP-KC60VS9\kdy <author>
2021-12-13 17:23:09 +0900

    BOJ 13164 행복유치원

---
+++ 22주차/BOJ 13164/BOJ_13164_kdy.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N13164 {// BOJ 13164. 행복유치원
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] h = new int[N];
+		st = new StringTokenizer(br.readLine());
+		h[0] = Integer.parseInt(st.nextToken());
+		int[] dif = new int[N];
+		for (int i = 1; i < N; i++) {
+			h[i] = Integer.parseInt(st.nextToken());
+			dif[i] = h[i] - h[i - 1];
+		}
+		Arrays.sort(dif);
+		int ans = 0;
+		for (int i = 0; i <= N - K; i++) {
+			ans += dif[i];
+		}
+		System.out.println(ans);
+	}
+
+}

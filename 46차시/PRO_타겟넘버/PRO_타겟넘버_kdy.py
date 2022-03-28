3188721e9d1acdc83596564eeae4de291ff076f0
김다예 <ek03147@gmail.com>
2022-03-28 20:18:31 +0900

    [46주차] 타겟넘버 코드 수정

--- 46차시/PRO_타겟넘버/PRO_타겟넘버_kdy.py
+++ 46차시/PRO_타겟넘버/PRO_타겟넘버_kdy.py
@@ -1,16 +1,10 @@
 def solution(numbers, target):
-    answer = 0
-    
-    q = [[numbers[0], 0], [-1*numbers[0], 0]]
-    
-    while q:
-        n, idx = q.pop()
-        idx +=1
-        if idx>=len(numbers):
-            if n == target:
-                answer += 1
-        else:
-            q.append([n + numbers[idx], idx])
-            q.append([n - numbers[idx], idx])
-
-    return answer
+    q = [0]
+    for n in numbers:
+        tmp = []
+        while q:
+            num = q.pop()
+            tmp.append(num-n)
+            tmp.append(num+n)
+        q+=tmp
+    return q.count(target)

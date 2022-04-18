0d7ad4fcb4bc58fc05c4532d1b546013af6ecfd9
김다예 <ek03147@gmail.com>
2022-04-18 20:55:47 +0900

    modify 50차시 톱니바퀴

--- 50차시/BOJ_톱니바퀴/BOJ_톱니바퀴_kdy.py
+++ 50차시/BOJ_톱니바퀴/BOJ_톱니바퀴_kdy.py
@@ -1,33 +1,24 @@
-# 극이 다르면 반대방향으로 회전
-# 극이 같으면 회전 X
-from collections import deque
+#BOJ 14891. 톱니바퀴
 
-
-def BFS(idx, direct):
-    visited[idx] = 1
-    # 오른쪽 방문
-    if idx < 3 and not visited[idx+1]:
-        if gear[idx][2] != gear[idx+1][6]:
-            BFS(idx+1, -direct)
-    # 왼쪽 방문
-    if idx > 0 and not visited[idx-1]:
-        if gear[idx][6] != gear[idx-1][2]:
-            BFS(idx-1, -direct)
-
-    # 회전 처리
-    if direct == 1:
-        gear[idx].appendleft(gear[idx].pop())
-    else:
-        gear[idx].append(gear[idx].popleft())
-
-gear = [deque(input()) for _ in range(4)]
+arr = [list(map(int, list(input()))) for _ in range(4)]
 K = int(input())
-answer = 0
-for _ in range(K):
-    idx, direct = map(int, input().split())
-    visited = [0] * 4
-    BFS(idx-1, direct)
-for i in range(4):
-    if gear[i][0] == '1':
-        answer += 1 << i
-print(answer)
+top = [0 for _ in range(4)]
+for k in range(K):
+    num, dir = map(int, input().split())
+    d = [0 for _ in range(4)]
+    d[num - 1] = dir
+    for i in range(num, 4):
+        if arr[i][(top[i] + 6) % 8] != arr[i - 1][(top[i - 1] + 2) % 8]:
+            d[i] = d[i - 1] * (-1)
+        else:
+            break
+    for i in range(num - 1, 0, -1):
+        if arr[i - 1][(top[i - 1] + 2) % 8] != arr[i][(top[i] + 6) % 8]:
+            d[i - 1] = d[i] * (-1)
+        else:
+            break
+    for i, dd in enumerate(d):
+        if dd == 0:
+            continue
+        top[i] = (top[i] - dd + 8) % 8
+print(arr[0][top[0]] + arr[1][top[1]] * 2 + arr[2][top[2]] * 4 + arr[3][top[3]] * 8)
